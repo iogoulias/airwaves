0b58f2eff8f8e60f272591efd51985b0038e87a7
Russell Bishop <hey@russellbishop.co.uk>
2021-12-06 21:37:34 +0000

    Metadata prefixes and syntax cleaning!

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var jsmediatags = require("jsmediatags");
-var fs=require("fs");
+var fs = require("fs");
 const express = require("express");
 
 var Airtable = require('airtable');
-var base= new Airtable({apiKey:'keyll8LpyyN3adHhR'}).base('app6GHxtqv6bp7oBk');
+var base = new Airtable({ apiKey: 'keyll8LpyyN3adHhR' }).base('app6GHxtqv6bp7oBk');
 
 var bodyParser = require('body-parser');
 var axios = require('axios');
@@ -12,121 +12,121 @@ const path = require('path');
 var qs = require('qs');
 const app = express();
 let port = process.env.PORT || 3000;
-const btoa = function(str){ return Buffer.from(str, 'binary').toString('base64'); };
-app.get('/page', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+const btoa = function (str) { return Buffer.from(str, 'binary').toString('base64'); };
+app.get('/page', function (req, res) {
+	res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.use(cors({origin: 'http://localhost:3000/*'}));
-app.use(cors({origin: 'https://iogoulias.github.io'}))
-app.use(cors({origin: '*'}))
+app.use(cors({ origin: 'http://localhost:3000/*' }));
+app.use(cors({ origin: 'https://iogoulias.github.io' }))
+app.use(cors({ origin: '*' }))
 app.use(bodyParser.urlencoded({
-  extended: true
+	extended: true
 }));
-app.get('/image', function(req, res) {
+app.get('/image', function (req, res) {
 	console.log("requested");
-  res.sendFile(path.join(__dirname, '/'+req.query.file));
+	res.sendFile(path.join(__dirname, '/' + req.query.file));
 });
-app.get("/artwork", function(req, res) {
-	var id=req.query.id;
-	var file=req.query.file;
-	jsmediatags.read("https://docs.google.com/uc?export=download&id="+id, {
-  onSuccess: async function(tag) {
-    
-	var image=null;
-	if ((tag.tags).hasOwnProperty("picture")) {
-		console.log('has picture');
-	image = tag.tags.picture;	
-	}
-	var artist=null;
-	if ((tag.tags).hasOwnProperty("artist")) {
-		console.log('has artist');
-	artist = tag.tags.artist;	
-	}
-	var album=null;
-	if ((tag.tags).hasOwnProperty("album")) {
-		console.log('has album');
-	album = tag.tags.album;	
-	}
-	var title=null;
-	if ((tag.tags).hasOwnProperty("title")) {
-		console.log('has title');
-	title = tag.tags.title;	
-	}
-	// var duration=null;
-	// if ((tag.tags).hasOwnProperty("TLEN") && (tag.tags.TLEN).hasOwnProperty("data") ) {
-	// duration = parseFloat(tag.tags.TLEN.data);	
-	// }
-	//   console.log("duration : "+duration)
-      if (image!=null) {
-		  console.log('has image');
-	 var writeformat=((image.format).split("/"))[1];
-	 if (writeformat=="jpeg") {	 
-		 writeformat="jpg";
-	 }
-	 fs.writeFile(id+"."+writeformat,Buffer.from(image.data),"binary",async function(){
-		var temppath=path.dirname(require.main.filename)
-		// cloudinary.v2.uploader.upload(id+"."+writeformat,
-      //  { public_id: id }, 
-       //  function(error, result) {console.log(result.secure_url);
-       //  res.send(airtable({"File ID":id,"File Name":file,"Album Artwork":[{"url":result.secure_url}],"Artist":artist,"Album":album,"Track Name":title,"Duration":duration},id,true));	 
-console.log(temppath+"\\"+id+"."+writeformat)
-var tempresp=await airtable({"File ID":id,"File Name":file,"Album Artwork":[{"url":"https://airwaves-rw4kx.ondigitalocean.app/image?file="+id+"."+writeformat}],"Artist":artist,"Album":album,"Track Name":title},id,true);
-console.log(tempresp)
-res.send(JSON.stringify(tempresp));	 
-		 
-		// });
-		 
-		 
-	 })
-      } else {
-	      var tempresp=await airtable({"File ID":id,"File Name":file,"Album Artwork":null,"Artist":artist,"Album":album,"Track Name":title},id,false)
-		res.send(JSON.stringify(tempresp));
-      }
-},
-  onError: function(error) {
-    console.log(':(', error.type, error.info);
-    res.send(JSON.stringify({"result":"unexpected error"}));
-  }
-});
-      
+app.get("/artwork", function (req, res) {
+	var id = req.query.id;
+	var file = req.query.file;
+	jsmediatags.read("https://docs.google.com/uc?export=download&id=" + id, {
+		onSuccess: async function (tag) {
+
+			var image = null;
+			if ((tag.tags).hasOwnProperty("picture")) {
+				console.log('has picture');
+				image = tag.tags.picture;
+			}
+			var artist = null;
+			if ((tag.tags).hasOwnProperty("artist")) {
+				console.log('has artist');
+				artist = tag.tags.artist;
+			}
+			var album = null;
+			if ((tag.tags).hasOwnProperty("album")) {
+				console.log('has album');
+				album = tag.tags.album;
+			}
+			var title = null;
+			if ((tag.tags).hasOwnProperty("title")) {
+				console.log('has title');
+				title = tag.tags.title;
+			}
+			
+			if (image != null) {
+				console.log('has image');
+				var writeformat = ((image.format).split("/"))[1];
+				if (writeformat == "jpeg") {
+					writeformat = "jpg";
+				}
+				fs.writeFile(id + "." + writeformat, Buffer.from(image.data), "binary", async function () {
+					var temppath = path.dirname(require.main.filename)
+					
+					console.log(temppath + "\\" + id + "." + writeformat)
+					var tempresp = await airtable(
+						{ 
+							"Metadata: File ID": id, 
+							"Metadata: File Name": file, 
+							"Album Artwork": [{ "url": "https://airwaves-rw4kx.ondigitalocean.app/image?file=" + id + "." + writeformat }], 
+							"Metadata: Artist": artist, 
+							"Metadata: Artist": album, 
+							"Metadata: Track Name": title 
+						}, 
+						id, 
+						true
+					);
+					console.log(tempresp)
+					res.send(JSON.stringify(tempresp));
+
+				})
+			} else {
+				var tempresp = await airtable(
+					{ 
+						"Metadata: File ID": id, 
+						"Metadata: File Name": file, 
+						"Album Artwork": null, 
+						"Metadata: Artist": artist, 
+						"Metadata: Album": album, 
+						"Metadata: Track Name": title 
+					}, 
+					id, 
+					false
+				)
+				res.send(JSON.stringify(tempresp));
+			}
+		},
+		onError: function (error) {
+			console.log(':(', error.type, error.info);
+			res.send(JSON.stringify({ "result": "unexpected error" }));
+		}
+	});
+
 });
 
 app.listen(port, () => {
-       console.log('Example app is listening on port http://localhost:'+port)
+	console.log('Example app is listening on port http://localhost:' + port)
 });
 
-async function airtable(fields,id,hasimage) {
-console.log("called airtable");
-return new Promise((resolve, reject) => {
+async function airtable(fields, id, hasimage) {
+	console.log("called airtable");
+	return new Promise((resolve, reject) => {
 
-      // Handle resolve and reject in the asynchronous API
-  base('Tracks').create([
-      {
-      "fields": fields
-   }
-   ], function(err, records) {
-   if (err) {
-console.log(fields["File Name"])
-    console.error(err);
-    resolve( {"result":"not ok"});
-  } else {
-  resolve( {"result":"ok"});
-  }
-  });
+		// Handle resolve and reject in the asynchronous API
+		base('Tracks').create([
+			{
+				"fields": fields
+			}
+		], function (err, records) {
+			if (err) {
+				console.log(fields["File Name"])
+				console.error(err);
+				resolve({ "result": "not ok" });
+			} else {
+				resolve({ "result": "ok" });
+			}
+		});
 
-  })
-  
+	})
+
 }
-
-
-
-
-// const cloudinary = require("cloudinary");
-
-
-// cloudinary.config({ 
-//   cloud_name: 'dxpigklxv', 
-//   api_key: '463342247927238', 
-//   api_secret: 'IO4rR5Crlj4xoepb0cCDwUnKkeU' 
-// });
